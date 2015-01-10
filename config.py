88c9f990d4dcdd86410aa2594483e15558255049
Lars Kiesow <lkiesow@uos.de>
2015-01-11 00:37:44 +0100

    Added User Management
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

---
+++ config.py
@@ -0,0 +1,13 @@
+
+##
+# Users with write access to the wiki
+# Format: [(<username>, <salt>, <hashed password>), ...]
+# To generate a hashed password you may use:
+#   python -c 'import hashlib; print(hashlib.sha512("SALT"+"PASSWORD").hexdigest())'
+##
+USER = [
+		('default','234','baf309cb231d587e3b98a034da6c5087213d67b287f781b25f7eaa9dd537331b9fac18b5dbac1110f1cc3636dc79af1bad73eb77587958f01839bbc65b989813'),
+		('lk','gsrsdf','baf309cb231d587e3b98a034da6c5087213d67b287f781b25f7eaa9dd537331b9fac18b5dbac1110f1cc3636dc79af1bad73eb77587958f01839bbc65b989813')
+		]
+
+USER=[]
--- wikimd.py
+++ wikimd.py
@@ -9,8 +9,12 @@ sys.setdefaultencoding('utf8')
 import os, errno
 from markdown2 import markdown
 from subprocess import Popen
-from flask import Flask, render_template, request, Response, redirect
+from hashlib import sha512
+from flask import Flask, render_template, request, Response, redirect, session, url_for
 app = Flask(__name__)
+app.secret_key = 'LHlhdeuhlkeuKJHLuwhdlajLz7ZO7h3ohdo3o'
+
+import config
 
 DATADIR = '%s/data' % os.path.dirname(os.path.realpath(__file__))
 
@@ -40,7 +44,7 @@ def home(path=''):
 	pathsegments = path.strip('/').split('/')
 	pathsegments = pathsegments if pathsegments[0] else pathsegments[1:]
 	breadcrum = [('⌂','/')] \
-			+ [ (pathsegments[i], '/' + '/'.join(pathsegments[:i+1])) 
+			+ [ (pathsegments[i], '/' + '/'.join(pathsegments[:i+1]))
 					for i in range(len(pathsegments)) ]
 
 	path = '%s/%s' % (DATADIR, path)
@@ -52,16 +56,35 @@ def home(path=''):
 	if os.path.isfile(path):
 		with open(path, 'r') as f:
 			content = f.read()
+
+	# Navigation we want to show
+	nav = []
+	if session.get('user'):
+		nav = ('logout','edit')
+	elif config.USER:
+		nav = ('login',)
+	else:
+		nav = ('edit',)
+
 	if content and request.args.get('edit') is None:
-		return render_template('wiki.html', path=request.path,
+		return render_template('wiki.html', path=request.path, nav=nav,
 				content=markdown(content), breadcrum=breadcrum)
-	return render_template('editor.html', path=request.path, content=content,
-			breadcrum=breadcrum)
+	if session.get('user') or not config.USER:
+		return render_template('editor.html', path=request.path, content=content,
+				breadcrum=breadcrum, nav=nav)
+	if not request.args.get('edit') is None:
+		return '', 404
+	return render_template('wiki.html', path=request.path, nav=nav,
+			content='No content', breadcrum=breadcrum)
 
 
 @app.route("/", methods=['POST'])
 @app.route("/<path:path>", methods=['POST'])
 def save(path=''):
+
+	# Check if user is allowed to edit
+	if config.USER and not session.get('user'):
+		return '', 404
 
 	path = '%s/%s' % (DATADIR, path)
 
@@ -80,6 +103,25 @@ def save(path=''):
 
 	# Return page
 	return redirect(request.path)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+	user = [u for u in config.USER if u[0] == request.form.get('user')]
+	if not user:
+		return redirect(url_for('home'))
+	user, salt, passwd = user[0]
+	if passwd != sha512(salt + request.form.get('password')).hexdigest():
+		return redirect(url_for('home'))
+	session['user'] = user
+	return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def logout():
+	# remove user from session if it's there
+	session.pop('user', None)
+	return redirect(url_for('home'))
 
 
 init()
